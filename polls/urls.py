161bb0606681781c2cc12758bbf7f8c381172247
zuber shekh <zuber.shekh7@gmail.com>
2020-05-21 18:48:28 +0530

    added index and detail view

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('detail/<int:question_id>',views.detail,name='detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Question
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("polls apps")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list':latest_question_list,
+    }
+    return render(request,'polls/index.html',context)
+
+def detail(request,question_id):
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question':question,
+    }
+    return render(request,'polls/detail.html',context)
+
+
+def results(request):
+    return ""
+
+
+def vote(request):
+    return ""
